2241c472705efb88cad37d2e4cf502c6c6d3d8ae
TRONG <tronginc@gmail.com>
2018-11-01 14:18:57 +0700

    Comment SignIn Api

--- Devices Control Program/Source/Util/Api.cs
+++ Devices Control Program/Source/Util/Api.cs
@@ -14,15 +14,19 @@ namespace Devices_Control_Program.Source.Util
 {
     class Api
     {
+        // Phương thức đăng nhập, truyền 2 tham số username và password
         public static bool SignIn(String username, String password)
         {
             HttpWebResponse httpResponse = null;
             try
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://thomi.com.vn:8080/auth");
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://125.212.254.10:8080/auth");
+
+                // Phần Headers
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
 
+                // Phần Body
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
                     string json = "{\"username\":\"" + username + "\"," +
@@ -33,6 +37,7 @@ namespace Devices_Control_Program.Source.Util
                     streamWriter.Close();
                 }
 
+                // Phần response
                 httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
@@ -40,9 +45,10 @@ namespace Devices_Control_Program.Source.Util
                     JObject jObject = JObject.Parse(result);
                     Debug.WriteLine(jObject.GetValue("message"));
                     Data.User.token = jObject.GetValue("message").ToString();
-                    return true;
+                    return true; // Trả kết quả - đăng nhập thành công
                 }
             }
+            // Bắt các Exception
             catch (WebException e)
             {
                 if (e.Status == WebExceptionStatus.ProtocolError)
@@ -50,11 +56,14 @@ namespace Devices_Control_Program.Source.Util
                     httpResponse = (HttpWebResponse)e.Response;
                     Debug.Write("Errorcode: {0}", httpResponse.StatusCode.ToString());
                     using (var stream = e.Response.GetResponseStream())
+
+                    // Lấy thông tin lỗi từ server
                     using (var reader = new StreamReader(stream))
                     {
                         var result = reader.ReadToEnd();
                         JObject jObject = JObject.Parse(result);
                         Debug.WriteLine(jObject.GetValue("errMessage").ToString());
+                        // Throw lỗi để ra bên ngoài bắt
                         throw new Exception(jObject.GetValue("errMessage").ToString());
 
                     }
@@ -64,11 +73,12 @@ namespace Devices_Control_Program.Source.Util
                     Debug.Write("Error: {0}", e.Status.ToString());
                 }
             }
+            // Kết thúc request
             finally
             {
                 if (httpResponse != null)
                 {
-                    httpResponse.Close();                    
+                    httpResponse.Close();
                 }
             }
             return false;
@@ -79,7 +89,7 @@ namespace Devices_Control_Program.Source.Util
             HttpWebResponse httpResponse = null;
             try
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://thomi.com.vn:8080/room/list");               
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://125.212.254.10:8080/room/list");
                 httpWebRequest.Method = "GET";
                 httpWebRequest.Headers["Authorization"] = "Bearer " + Data.User.token;
 
@@ -87,7 +97,7 @@ namespace Devices_Control_Program.Source.Util
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
                     var result = streamReader.ReadToEnd();
-                    Debug.WriteLine(result);                                     
+                    Debug.WriteLine(result);
                     var rooms = JsonConvert.DeserializeObject<List<Room>>(result);
                     Data.User.rooms = rooms;
                     return true;
@@ -128,7 +138,7 @@ namespace Devices_Control_Program.Source.Util
             HttpWebResponse httpResponse = null;
             try
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://thomi.com.vn:8080/device/list");               
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://125.212.254.10:8080/device/list");
                 httpWebRequest.Method = "GET";
                 httpWebRequest.Headers["Authorization"] = "Bearer " + Data.User.token;
 
@@ -137,8 +147,8 @@ namespace Devices_Control_Program.Source.Util
                 {
                     var result = streamReader.ReadToEnd();
                     Debug.WriteLine(result);
-                    var devices = JsonConvert.DeserializeObject<List<Device>>(result);                   
-                    Data.User.devices = devices;                    
+                    var devices = JsonConvert.DeserializeObject<List<Device>>(result);
+                    Data.User.devices = devices;
                     return true;
                 }
             }
@@ -177,7 +187,7 @@ namespace Devices_Control_Program.Source.Util
             HttpWebResponse httpResponse = null;
             try
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://thomi.com.vn:8080/room/add");
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://125.212.254.10:8080/room/add");
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
                 httpWebRequest.Headers["Authorization"] = "Bearer " + Data.User.token;
@@ -191,6 +201,49 @@ namespace Devices_Control_Program.Source.Util
                     streamWriter.Close();
                 }
 
+                httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                if ((int)httpResponse.StatusCode == 200) return true;
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    httpResponse = (HttpWebResponse)e.Response;
+                    Debug.Write("Errorcode: {0}", httpResponse.StatusCode.ToString());
+                    using (var stream = e.Response.GetResponseStream())
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var result = reader.ReadToEnd();
+                        JObject jObject = JObject.Parse(result);
+                        Debug.WriteLine(jObject.GetValue("errMessage").ToString());
+                        throw new Exception(jObject.GetValue("errMessage").ToString());
+
+                    }
+                }
+                else
+                {
+                    Debug.Write("Error: {0}", e.Status.ToString());
+                }
+            }
+            finally
+            {
+                if (httpResponse != null)
+                {
+                    httpResponse.Close();
+                }
+            }
+            return false;
+        }
+
+        public static bool DeleteRoom(String id)
+        {
+            HttpWebResponse httpResponse = null;
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://125.212.254.10:8080/room/" + id);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "DELETE";
+                httpWebRequest.Headers["Authorization"] = "Bearer " + Data.User.token;
                 httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                 if ((int)httpResponse.StatusCode == 200) return true;
             }
